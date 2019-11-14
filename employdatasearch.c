4225d62ed8d0aa46f9972c1e0bbd97c9db36c376
deepakasd <54941221+deepakasd@users.noreply.github.com>
2019-11-14 06:23:21 +0530

    Update employdatasearch.c

--- employdatasearch.c
+++ employdatasearch.c
@@ -1,33 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<malloc.h>
+struct node{
+	int empid;char ename[20];float lt;
+	struct node*left;
+	struct node*right;
+};
+typedef struct node*NODE;
+  NODE insert(NODE root,int eid,char ename[20],float lt)
+  {
+	  NODE prev,pres;
+	  NODE newnode=(NODE)malloc(sizeof(struct node));
+	  newnode->left=newnode->right=NULL;
+	  newnode->empid=eid;
+	  strcpy(newnode->ename,ename);
+	  newnode->lt=lt;
+	  if(root==NULL)
+	  {
+		  root=newnode;
+		  return root;
+	  }
+	  pres=root;
+	  while(pres!=NULL)
+	  {
+		  prev=pres;
+		  if(eid<pres->empid)
+		  {
+			  pres=pres->left;
+		  }
+		else  if(eid==pres->empid)
+		  {
+			  printf("duplicte identifier\n");
+			  return root;
+		  }
+		  else
+		  pres=pres->right;
+	  }
+	  if(eid<prev->empid)
+	  prev->left=newnode;
+	  else
+	  prev->right=newnode;
+	  return root;
+  }
+ 
+  void inorder(NODE root)
+  {
+	  if(root!=NULL)
+	  {inorder(root->left);
+		  printf("%d\t+%s\t+%f\n",root->empid,root->ename,root->lt);
+		  inorder(root->right);
+	  }
+  }
+ 
+
 int main()
 {
 	NODE root=NULL;
-	int ch,item;
+	int ch,eid;char ename[20];
+	float lt;
 	for(;;)
 	{
-		printf("1:insert 2:pre 3:in 4:post\n");
+		printf("1:insert 2:inorder else:exit\n");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1:printf("enter the item\n");
-			       scanf("%d",&item);
-			       root=insert(root,item);
+			case 1:printf("enter the eid,ename,lt\n");
+			       scanf("%d%s%f",&eid,ename,&lt);
+			       root=insert(root,eid,ename,lt);
 			       break;
 			case 2:if(root==NULL)
 			       printf("empty tree\n");
 			       else
-			       preorder(root);
-			       break;
-			 case 3:if(root==NULL)
-			       printf("empty tree\n");
-			       else
 			       inorder(root);
 			       break;
-			 case 4:if(root==NULL)
-			        printf("empty tree");
-			        else
-			        postorder(root);
-			        break;
+			
 			  default:exit(0);
 		  }
 	  }
-}
+  }
